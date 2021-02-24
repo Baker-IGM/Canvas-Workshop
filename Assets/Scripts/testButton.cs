af5faaa92222a1ef97b98970145b5489e049bbee
Baker <eab6863@gmail.com>
2021-02-24 08:20:38 -0500

    Added button script

---
+++ Assets/Scripts/testButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SayHi()
+    {
+        Debug.Log("Hi!");
+    }
+
+    public void SaySomething(string phrase)
+    {
+        Debug.Log(phrase);
+    }
+
+    public void Repeat(int num)
+    {
+
+    }
+}
